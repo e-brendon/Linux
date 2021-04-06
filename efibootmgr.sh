a7ef908212cdb2bfbdf7c490e7a7cae4f7c71691
Brendon Esteves <51219353+Unckros@users.noreply.github.com>
2021-04-06 11:57:56 -0400

    Delete efibootmgr.sh

--- efibootmgr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-printf '\n[repo-ck] \nServer = https://mirror.lesviallon.fr/$repo/os/$arch \nServer = http://repo-ck.com/$arch' >> /etc/pacman.conf
-efibootmgr -q --disk /dev/nvme0n1 --part 1 --create --label "Arch Linux" --loader "\vmlinuz-linux-ck-skylake" --unicode 'root=PARTUUID="7f6cc65e-397b-ea4f-ac51-a8fe5b5e9ed0" rw initrd=\initramfs-linux-ck-skylake.img quiet rd.udev.log-priority=0 pci=noaer nowatchdog' --verbose
